1462161ac1576b304ca9719319a13468584d9fc4
rbral <rbral@student.touro.edu>
2024-05-08 17:03:08 -0400

    fixed

--- src/main/java/bral/earthquake/EarthquakeFrame.java
+++ src/main/java/bral/earthquake/EarthquakeFrame.java
@@ -124,7 +124,8 @@ public class EarthquakeFrame extends JFrame {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         new EarthquakeFrame().setVisible(true);
     }
 
